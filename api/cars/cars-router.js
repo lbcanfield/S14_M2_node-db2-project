230c65724c08f047893119383ae8653038f2668a
lbcanfield <lewie.b.canfield@gmail.com>
2022-12-28 12:03:46 -0600

    start of cars-router.js fleshed out cars-router connected to server.js

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,22 @@
-// DO YOUR MAGIC
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (request, response, next) => {
+     console.log('get cars endpoint')
+})
+router.get('/:id', (request, response, next) => {
+     console.log('get specific car endpoint')
+})
+router.post('/', (request, response, next) => {
+     console.log('post endpoint')
+})
+router.put('/', (request, response, next) => {
+     console.log('update endpoint')
+})
+router.delete('/:id', (request, response, next) => {
+     console.log('delete endpoint')
+})
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require("express")
+const carsRouter = require('./cars/cars-router')
 
 const server = express()
 
 server.use(express.json())    //allows express to parse JSON
+server.use('/api/cars', carsRouter)
 
-// DO YOUR MAGIC
 
 server.use('*', (request, response) => {
      response.send("API is running")
