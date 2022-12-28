4a19dc7c3e57a807a691f3f3b0eb062c6a17cb05
lbcanfield <lewie.b.canfield@gmail.com>
2022-12-28 12:18:40 -0600

    cars-model.js - getAll() db function complete cars-router.js - get endpoint method complete

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,20 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+     return db('cars')
 }
 
 const getById = () => {
-  // DO YOUR MAGIC
+     // DO YOUR MAGIC
 }
 
 const create = () => {
-  // DO YOUR MAGIC
+     // DO YOUR MAGIC
+}
+
+
+module.exports = {
+     getAll,
+     getById,
+     create
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const CARS = require('./cars-model');
+const { } = require('./cars-middleware');
+
 const router = express.Router();
 
 
-router.get('/', (request, response, next) => {
-     console.log('get cars endpoint')
+router.get('/', async (request, response, next) => {
+     try {
+          const cars = await CARS.getAll()
+          response.json(cars)
+     }
+     catch (error) {
+          next(error)
+     }
 })
 router.get('/:id', (request, response, next) => {
      console.log('get specific car endpoint')
@@ -11,11 +20,19 @@ router.get('/:id', (request, response, next) => {
 router.post('/', (request, response, next) => {
      console.log('post endpoint')
 })
-router.put('/', (request, response, next) => {
-     console.log('update endpoint')
-})
-router.delete('/:id', (request, response, next) => {
-     console.log('delete endpoint')
+// router.put('/', (request, response, next) => {
+//      console.log('update endpoint')
+// })
+// router.delete('/:id', (request, response, next) => {
+//      console.log('delete endpoint')
+// })
+
+// Error Handling MiddleWare
+router.use((error, request, response, next) => {
+     response.status(error.status || 500).json({
+          message: error.message,
+          stack: error.stack
+     })
 })
 
 
