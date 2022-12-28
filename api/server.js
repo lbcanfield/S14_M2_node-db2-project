6d7c761a03d534442841122e636c773cb1800718
lbcanfield <lewie.b.canfield@gmail.com>
2022-12-28 11:52:01 -0600

    installed dotenv modified index.js to consume .env file data created scripts in package.json file connected server

--- api/server.js
+++ api/server.js
@@ -2,6 +2,12 @@ const express = require("express")
 
 const server = express()
 
+server.use(express.json())    //allows express to parse JSON
+
 // DO YOUR MAGIC
+
+server.use('*', (request, response) => {
+     response.send("API is running")
+})
 
 module.exports = server
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const server = require('./api/server')
 
 const port = process.env.PORT || 9000;
+const host = process.env.HOSTNAME || 'http://localhost'
 
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`))
+server.listen(port, () => console.log(`\n** Running on ${host}:${port} **\n`))
